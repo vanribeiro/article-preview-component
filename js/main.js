aafea4746ca5ee84e06a5fa520ab91467348e01f
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-08-26 15:54:13 -0300

    finish challenge. Need fixes.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-const button = document.querySelector('.share-icon');
+const enableShareIconButton = document.querySelector('.share-icon');
+const disableShareIconButton = document.querySelector('.share-buttons .share-icon');
 const socialMediaContainer = document.querySelector('.share-buttons');
 
 
-const handleMouseIn = () => {
-    socialMediaContainer.classList.add('share-buttons--toggle');
-}
-const handleMouseOut = () => {
-    socialMediaContainer.classList.remove('share-buttons--toggle');
-}
+const handleMouseIn = () => socialMediaContainer.classList.add('share-buttons--toggle');
+const handleMouseOut = () => socialMediaContainer.classList.remove('share-buttons--toggle');
 
-button.addEventListener('mouseenter', handleMouseIn);
-button.addEventListener('mouseleave', handleMouseOut);
+const handleTouchIn = () => socialMediaContainer.classList.add('share-buttons--toggle');
+const handleTouchOut = (e) => socialMediaContainer.classList.remove('share-buttons--toggle');
+
+enableShareIconButton.addEventListener('mouseenter', handleMouseIn);
+enableShareIconButton.addEventListener('mouseleave', handleMouseOut);
+enableShareIconButton.addEventListener('click', handleTouchIn);
+disableShareIconButton.addEventListener('click', handleTouchOut);
