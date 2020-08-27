f81edeba3e0e36d9e1010e9b425d86cdef1a5dce
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-08-27 15:35:03 -0300

    fix accessility.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const enableShareIconButton = document.querySelector('.share-icon');
+const enableShareIconButton = document.querySelector('.share-section');
 const disableShareIconButton = document.querySelector('.share-buttons .share-icon');
 const socialMediaContainer = document.querySelector('.share-buttons');
 
@@ -6,10 +6,22 @@ const socialMediaContainer = document.querySelector('.share-buttons');
 const handleMouseIn = () => socialMediaContainer.classList.add('share-buttons--toggle');
 const handleMouseOut = () => socialMediaContainer.classList.remove('share-buttons--toggle');
 
+const handleTabIn = (e) => {
+    if(e.keyCode === 13) socialMediaContainer.classList.add('share-buttons--toggle');
+}
+
+const handleTabOut = (e) => {
+    if(e.keyCode === 27) socialMediaContainer.classList.remove('share-buttons--toggle');
+}
+
 const handleTouchIn = () => socialMediaContainer.classList.add('share-buttons--toggle');
-const handleTouchOut = (e) => socialMediaContainer.classList.remove('share-buttons--toggle');
+const handleTouchOut = () => socialMediaContainer.classList.remove('share-buttons--toggle');
 
 enableShareIconButton.addEventListener('mouseenter', handleMouseIn);
 enableShareIconButton.addEventListener('mouseleave', handleMouseOut);
+
+enableShareIconButton.addEventListener('keydown', handleTabIn);
+enableShareIconButton.addEventListener('keydown', handleTabOut);
+
 enableShareIconButton.addEventListener('click', handleTouchIn);
 disableShareIconButton.addEventListener('click', handleTouchOut);
